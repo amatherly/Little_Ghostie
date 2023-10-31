68e5f386841007aaa7d8fd801045be9ee9bad313
Sispuria67 <99496880+Sispuria67@users.noreply.github.com>
2023-10-30 17:24:11 -0700

    Put randomized key spawns.

---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject keyPrefab;
+    [SerializeField]public Transform[] spawnLocations;
+ 
+  
+
+    void Start()
+    {
+        
+           
+            Instantiate(keyPrefab, spawnLocations[Random.Range(1, 4)].position, Quaternion.identity); 
+        
+    }
+
+   
+}
