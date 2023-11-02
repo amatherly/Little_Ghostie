d1ed469326928e69a3be8f197ffcd221abf05fd7
BRODI MATHERLY <69179454+amatherly@users.noreply.github.com>
2023-11-02 10:39:16 -0700

    Added Healthbar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using JetBrains.Annotations;
 
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,26 +16,26 @@ public class PlayerController : MonoBehaviour
     }
 
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float amplitude = 0.5f; 
-    [SerializeField] private List<Sprite> sprites;
+    [SerializeField] private float amplitude = 0.5f;
     [SerializeField] private float speed = 1f; 
     [SerializeField] private Light2D candle;
-    [SerializeField] private float damage = 0.2f; 
+    [SerializeField] private float damage = 1f; 
+    [SerializeField] private int health = 7;
 
 
-
+    [FormerlySerializedAs("moveSound")] [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip sizzleSound;
     private Rigidbody2D rb;
     private Sprite currSprite;
-    [CanBeNull] private AudioSource moveSound;
+
     private float verticalDelta = 0f;
-    [SerializeField] private int health = 7;
     private bool hasKey = false;
 
     private void Awake()
     {
         currSprite = GetComponent<Sprite>();
         rb = GetComponent<Rigidbody2D>();
-        moveSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -54,13 +55,13 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity = newVelocity;
 
-        if (moveDirection.magnitude > 0 && !moveSound.isPlaying)
+        if (moveDirection.magnitude > 0 && !audioSource.isPlaying)
         {
-            moveSound.UnPause();
+            audioSource.UnPause();
         }
         else
         {
-            moveSound.Pause();
+            audioSource.Pause();
         }
         
         Debug.Log("Player has key: " + hasKey);
@@ -71,6 +72,30 @@ public class PlayerController : MonoBehaviour
         health -= points;
         UpdateSprite(health);
     }
+    
+    private void UpdateSprite(int candleLevel)
+    {
+        if (candleLevel > 0)
+        {
+            candle.pointLightOuterRadius-= damage;
+            candle.pointLightInnerRadius-= damage;
+            FindObjectOfType<Candle>().UpdateHealthbar();
+            audioSource.PlayOneShot(sizzleSound);
+            Debug.Log("Sprite updated, candle level: " + candleLevel);
+        }
+        else
+        {
+            GameOver();
+            Debug.Log("Player died");
+        }
+    }
+    
+        
+    public void GameOver()
+    {
+        FindObjectOfType<UI>().GameOver();
+        Time.timeScale = 0;
+    }
 
     private IEnumerator FloatUpAndDown()
     {
@@ -80,27 +105,6 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
     }
-
-    private void UpdateSprite(int candleLevel)
-    {
-        if (candleLevel > 0)
-        {
-            currSprite = sprites[candleLevel];
-            candle.pointLightOuterRadius-= damage;
-            candle.pointLightInnerRadius-= damage;
-            Debug.Log("Sprite updated, candle level: " + candleLevel);
-        }
-        else
-        {
-            GameOver();
-            Debug.Log("Player died");
-        }
-    }
-
-    public void GameOver()
-    {
-        FindObjectOfType<UI>().GameOver();
-        Time.timeScale = 0;
-    }
+    
     
 }
